3b95e365e42a5d9a8d61646c95a59882f8f7245c
Anastasia-98Moiseeva <nepro100nastya@yandex.ru>
2021-11-13 16:37:24 +0300

    fix hw5

--- hw5/advert.py
+++ hw5/advert.py
@@ -1,40 +1,63 @@
 import json
+import keyword
 
 
 class ColorizeMixin:
     repr_color_code = 32
 
     def __repr__(self):
-        format_code = f'\033[0;{self.repr_color_code};1m'
-        return format_code
+        text = super().__repr__()
+        return f'\033[0;{self.repr_color_code};1m{text}'
 
 
-class Advert(ColorizeMixin):
-
-    def __init__(self, json_dict):
-        self.price = 0
-        for key in json_dict.keys():
-            if isinstance(json_dict[key], dict):
-                self.__dict__[key] = Advert(json_dict[key])
-            else:
-                self.__dict__[key] = json_dict[key]
-        if self.price < 0:
-            raise ValueError('must be >= 0')
-
+class BaseAdvert:
     def __repr__(self):
-        return super().__repr__() + f'{self.title} | {self.price} ₽'
+        return f'{self.title} | {self.price} ₽'
+
+
+class JsonObject:
+    def __init__(self, json_dict):
+        for key, value in json_dict.items():
+            if isinstance(value, dict):
+                value = JsonObject(value)
+            if key in keyword.kwlist:
+                setattr(self, f'{key}_', value)
+            else:
+                setattr(self, key, value)
+
+
+class Advert(ColorizeMixin, JsonObject, BaseAdvert):
+    def __init__(self, json_dict):
+        if 'title' not in json_dict.keys():
+            raise AttributeError('title does not exists')
+        if 'price' not in json_dict.keys():
+            setattr(self, 'price', 0)
+        super().__init__(json_dict)
+
+    @property
+    def price(self):
+        return self.price_
+
+    @price.setter
+    def price(self, val):
+        if val < 0:
+            raise ValueError('must be >= 0')
+        self.price_ = val
 
 
 if __name__ == '__main__':
     lesson_str = """{ 
-                    "title": "python", 
-                    "price": 1, 
+                    "title": "python",
+                    "price": 1,
                     "location": { 
                         "address": "город Москва, Лесная, 7", 
                         "metro_stations": ["Белорусская"] 
-                        }
+                        },
+                    "class": "dog"
                     }"""
     lesson = json.loads(lesson_str)
     lesson_ad = Advert(lesson)
     print(lesson_ad.location.address)
+    print(lesson_ad.price)
+    print(lesson_ad.class_)
     print(lesson_ad)
